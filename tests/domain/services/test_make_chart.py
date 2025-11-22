0bdc924729a9fa08eb6f516f57d048c81c71db4f
Jeanneveev <gjjones5425@gmail.com>
2025-11-21 23:57:28 -0500

    feat: added updated tests for Chart

---
+++ tests/domain/services/test_make_chart.py
@@ -0,0 +1,148 @@
+import unittest
+from src.domain.services.make_chart import Chart
+from src.domain.services.make_pattern import ExpandedRow, Pattern
+from src.domain.model.model import Stitch
+
+class TestChart(unittest.TestCase):
+    def test_can_get_symbols_of_right_side_row(self):
+        row = ExpandedRow(1, [Stitch("k"), Stitch("p"), Stitch("k"), Stitch("p")], 4)
+        pattern = Pattern([row])
+        chart = Chart(pattern)
+
+        expected = {1: ["-", " ", "-", " "]}
+        actual = chart.get_row_symbols(1)
+
+        self.assertEqual(expected, actual)
+    
+    def test_can_get_symbols_of_wrong_side_row(self):
+        row_1 = ExpandedRow(1, [Stitch("k"), Stitch("yo"), Stitch("k")], 2)
+        # print(f"End stitch count is: {row_1.end_st_count}")
+        row_2 = ExpandedRow(2, [Stitch("k"), Stitch("k2tog")], 3)
+        pattern = Pattern([row_1, row_2])
+        chart = Chart(pattern)
+
+        expected = {2: ["-", "/"]}
+        actual = chart.get_row_symbols(2)
+
+        self.assertEqual(expected, actual)
+
+    def test_can_generate_border_of_maximum_row_length(self):
+        pattern = Pattern([
+            ExpandedRow(1, [Stitch("k"), Stitch("yo"), Stitch("k")], 2),
+            ExpandedRow(2, [Stitch("k"), Stitch("k"), Stitch("k")], 3)
+        ])
+        chart = Chart(pattern)
+
+        expected = "---+---+---+---+---\n"
+        actual = chart._build_border()
+
+        self.assertEqual(expected, actual)
+
+    def test_can_build_right_side_row(self):
+        pattern = Pattern([
+            ExpandedRow(1, [Stitch("k"), Stitch("yo"), Stitch("k")], 2),
+            ExpandedRow(2, [Stitch("k"), Stitch("k"), Stitch("k")], 3)
+        ])
+        chart = Chart(pattern)
+
+        expected = "   |   | O |   | 1 \n"
+        actual = chart._build_row(1)
+
+        self.assertEqual(expected, actual)
+
+    def test_can_build_wrong_side_row(self):
+        pattern = Pattern([
+            ExpandedRow(1, [Stitch("k"), Stitch("yo"), Stitch("k")], 2),
+            ExpandedRow(2, [Stitch("k"), Stitch("k"), Stitch("k")], 3)
+        ])
+        chart = Chart(pattern)
+
+        expected = " 2 | - | - | - |   \n"
+        actual = chart._build_row(2)
+
+        self.assertEqual(expected, actual)
+
+    def test_can_generate_1_row_chart_from_pattern(self):
+        pattern = Pattern([
+            ExpandedRow(1, [Stitch("k"), Stitch("k"), Stitch("p"), Stitch("p")], 4)
+        ])
+        chart = Chart(pattern)
+
+        expected = (
+            "---+---+---+---+---+---\n"
+            "   | - | - |   |   | 1 \n"
+            "---+---+---+---+---+---\n"
+        )
+        actual = chart.render_grid()
+
+        self.assertEqual(expected, actual)
+
+    def test_can_generate_multi_row_chart_from_pattern(self):
+        pattern = Pattern([
+            ExpandedRow(1, [
+                Stitch("k"), Stitch("k"), Stitch("p"), Stitch("p"),
+                Stitch("k"), Stitch("k"), Stitch("p"), Stitch("p"),
+                Stitch("k"), Stitch("k")
+            ], 10),
+            ExpandedRow(2, [
+                Stitch("k"),
+                Stitch("k"), Stitch("k"), Stitch("p"), Stitch("p"),
+                Stitch("k"), Stitch("k"), Stitch("p"), Stitch("p"),
+                Stitch("k")
+            ], 10)
+        ])
+        chart = Chart(pattern)
+
+        expected = (
+            "---+---+---+---+---+---+---+---+---+---+---+---\n"
+            " 2 | - | - | - |   |   | - | - |   |   | - |   \n"
+            "---+---+---+---+---+---+---+---+---+---+---+---\n"
+            "   |   |   | - | - |   |   | - | - |   |   | 1 \n"
+            "---+---+---+---+---+---+---+---+---+---+---+---\n"
+        )
+        actual = chart.render_grid()
+
+        self.assertEqual(expected, actual, f"expected was:\n{expected}\nactual was:\n{actual}")
+
+    def test_can_get_the_length_of_the_longest_column(self):
+        ex_col_contents = ["Name", "Knit", "Purl"]
+        junk_pattern = Pattern([ExpandedRow(1, [Stitch("k")], 1)])
+        chart = Chart(junk_pattern)
+
+        expected = 6
+        actual = chart._get_column_width(ex_col_contents)
+
+        self.assertEqual(expected, actual)
+
+    def test_can_pad_column_evenly(self):
+        ex_col_contents = ["Name", "Knit", "Purl"]
+        junk_pattern = Pattern([ExpandedRow(1, [Stitch("k")], 1)])
+        chart = Chart(junk_pattern)
+
+        expected = [" Name ", " Knit ", " Purl "]
+        actual = chart._pad_column(ex_col_contents)
+
+        self.assertEqual(expected, actual)
+
+    def test_can_generate_key_from_chart(self):
+        pattern = Pattern([
+            ExpandedRow(1, [Stitch("k"), Stitch("p"), Stitch("p"), Stitch("k")], 4),
+            ExpandedRow(2, [Stitch("p"), Stitch("k"), Stitch("k"), Stitch("p")], 4)
+        ])
+        chart = Chart(pattern)
+
+        expected = (
+            "+------+--------+-----------+-----------+\n"
+            "| Name | Abbrev | RS Symbol | WS Symbol |\n"
+            "+------+--------+-----------+-----------+\n"
+            "| Knit |    k   |           |     -     |\n"
+            "+------+--------+-----------+-----------+\n"
+            "| Purl |    p   |     -     |           |\n"
+            "+------+--------+-----------+-----------+\n"
+        )
+        actual = chart.render_key()
+
+        self.assertEqual(expected, actual)
+
+if __name__ == "__main__":
+    unittest.main()
